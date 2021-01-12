dfb0e0a852f7a677b25ae325ac573768540602eb
lwk <2049808824@qq.com>
2021-01-12 11:58:49 +0800

    aa

--- springcloud/cloud/cloud-provider-payment8001/src/main/java/com/gg/dao/PaymentDao.java
+++ springcloud/cloud/cloud-provider-payment8001/src/main/java/com/gg/dao/PaymentDao.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface PaymentDao {
 
-    public int create(Payment payment);
+    int create(Payment payment);
 
-    public Payment byId(@Param("id") Long id);
+    Payment byId(@Param("id") Long id);
 }
